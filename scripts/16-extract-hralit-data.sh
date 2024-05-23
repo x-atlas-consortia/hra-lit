104ed4aa201b6a12f3a98535954c857274a4db64
Michael Ginda <mginda@iu.edu>
2024-05-23 15:26:23 -0400

    Update export file name for jsonld files.

--- scripts/16-extract-hralit-data.sh
+++ scripts/16-extract-hralit-data.sh
@@ -7,7 +7,7 @@ source db-config.sh
 
 DIR=$RAW_DIR/$VERSION
 
-psql -t -A -f sql/60-extract-hralit/61-hraj_graph_jsonld.sql > $DIR/61-hralit-journals.jsonl
-psql -t -A -f sql/60-extract-hralit/62-hrap_graph_jsonld.sql > $DIR/62-hralit-persons.jsonl
-psql -t -A -f sql/60-extract-hralit/63-hrao_graph_jsonld.sql > $DIR/63-hralit-organizations.jsonl
-psql -t -A -f sql/60-extract-hralit/64-hraa_graph_jsonld.sql > $DIR/64-hralit-articles.jsonl
+psql -t -A -f sql/60-extract-hralit/61-hraj_graph_jsonld.sql > $DIR/hralit-journals.jsonl
+psql -t -A -f sql/60-extract-hralit/62-hrap_graph_jsonld.sql > $DIR/hralit-persons.jsonl
+psql -t -A -f sql/60-extract-hralit/63-hrao_graph_jsonld.sql > $DIR/hralit-organizations.jsonl
+psql -t -A -f sql/60-extract-hralit/64-hraa_graph_jsonld.sql > $DIR/hralit-articles.jsonl
