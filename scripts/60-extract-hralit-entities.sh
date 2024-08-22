98808957b33b88d670e737f1690b237b9e720352
Bruce W. Herr II <bherr@indiana.edu>
2024-08-22 15:22:06 -0400

    Rename

---
+++ scripts/60-extract-hralit-entities.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source constants.sh
+shopt -s extglob
+set -e
+
+source db-config.sh
+
+DIR=$RAW_DIR/$VERSION
+
+# Export data to jsonld format
+psql -t -A -f sql/60-extract-hralit-entities/61-hraj_graph_jsonld.sql > $DIR/hralit-journals.jsonl
+psql -t -A -f sql/60-extract-hralit-entities/62-hrap_graph_jsonld.sql > $DIR/hralit-persons.jsonl
+psql -t -A -f sql/60-extract-hralit-entities/63-hrao_graph_jsonld.sql > $DIR/hralit-organizations.jsonl
+psql -t -A -f sql/60-extract-hralit-entities/64-hraa_graph_jsonld.sql > $DIR/hralit-articles.jsonl
