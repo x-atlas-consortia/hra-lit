77f4b51917c0b21875b1c1fee305fc8ad92bbebd
Bruce W. Herr II <bherr@indiana.edu>
2024-08-22 15:11:03 -0400

    Rename

---
+++ scripts/10-extract-universe-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source constants.sh
+shopt -s extglob
+set -e
+
+source db-config.sh
+
+DIR=$RAW_DIR/$VERSION
+
+psql -t -A -f sql/10-extract-universe-data/10-publications.sql > $DIR/universe-publications.jsonl
+psql -t -A -f sql/10-extract-universe-data/20-pmid-mesh.sql > $DIR/universe-pmid-mesh.jsonl
