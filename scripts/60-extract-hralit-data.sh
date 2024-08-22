df809b4b895d9dce812c5792498b29fdd2910b9d
Bruce W. Herr II <bherr@indiana.edu>
2024-08-22 15:10:07 -0400

    A little more renaming for consistency

--- scripts/60-extract-hralit-data.sh
+++ scripts/60-extract-hralit-data.sh
@@ -8,7 +8,7 @@ source db-config.sh
 DIR=$RAW_DIR/$VERSION
 
 # Export data to jsonld format
-psql -t -A -f sql/60-extract-hralit/61-hraj_graph_jsonld.sql > $DIR/hralit-journals.jsonl
-psql -t -A -f sql/60-extract-hralit/62-hrap_graph_jsonld.sql > $DIR/hralit-persons.jsonl
-psql -t -A -f sql/60-extract-hralit/63-hrao_graph_jsonld.sql > $DIR/hralit-organizations.jsonl
-psql -t -A -f sql/60-extract-hralit/64-hraa_graph_jsonld.sql > $DIR/hralit-articles.jsonl
+psql -t -A -f sql/60-extract-hralit-data/61-hraj_graph_jsonld.sql > $DIR/hralit-journals.jsonl
+psql -t -A -f sql/60-extract-hralit-data/62-hrap_graph_jsonld.sql > $DIR/hralit-persons.jsonl
+psql -t -A -f sql/60-extract-hralit-data/63-hrao_graph_jsonld.sql > $DIR/hralit-organizations.jsonl
+psql -t -A -f sql/60-extract-hralit-data/64-hraa_graph_jsonld.sql > $DIR/hralit-articles.jsonl
